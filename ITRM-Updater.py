eedd7a67b6e123b59940464886707153a9553dd1
Jarek <jarektroyer@gmail.com>
2018-12-07 22:09:50 -0500

    Styling updates, bug fixes, addition of menu

--- ITRM-Updater.py
+++ ITRM-Updater.py
@@ -14,7 +14,6 @@ target_dir = "."
 '''
 To do:
 File name validation
-Duplicate handling
 More modularity
 Generalize
 '''
@@ -35,22 +34,36 @@ def csv_to_html(fp):
     #Opens target to the first non-header row of data
     f = open(fp, "rt")
     data = csv.reader(f)
-    skip_header = next( data )
+    skip_header = next(data)
+
+    #inserted when entries reach a new first letter (export db already sorts)
+    alphebetization_header = "<h2 style=\"#3c663b;margin:2.5%;\">{}</h2>\n\n"
+    #keeps track of our name's first letter for alphebetization headers
+    first_letter = False
 
     for row in data:
         #Attempts row-HTML conversion, stores if valid
         div = row_to_div(row,fp)
-        if div:
+        #Only run onversion if source data is sufficient and original
+        if div and div not in elements[fp]:
+            #If the first letter of our entries name is different, update then add marker to output
+            if (row[4])[0] != first_letter:
+                first_letter = (row[4])[0]
+                (elements[fp]).append(alphebetization_header.format(first_letter))
+            #Add entry to output
             (elements[fp]).append(div)
+
     #Clean up
     f.close()
 
 def row_to_div(row,fp):
     #row: [0]=url, [1]=zip, [2]=state, [3]=city, [4]=name (12/2/18)
     #          -style                    ||                        name                  ||                cty,st||zip       ||        url            ||               link_style-
-    entry = "<div {}>\n<h3 style=\"color:#3c663b;text-align:left;\">{}</h3>\n<h3 style=\"text-align:left;\">{}, {}\n{}</h3>\n<a href=\"{}\" target=\"_blank\" rel=\"noopener\" {}>{}</a></div>"
+    entry = "<div {}>\n<h3 style=\"color:#3c663b;text-align:left;\">{}</h3>\n<h3 style=\"text-align:left;\">{}, {}\n{}</h3>\n<a href=\"http://{}\" target=\"_blank\" {}>{}</a></div>\n\n"
     #                       -style                            ||                                name      ||      url               ||               link_style-
-    no_location_entry = "<div {}>\n<h3 style=\"color:#3c663b;text-align:left;margin-bottom:40%;\">{}</h3>\n<a href=\"{}\" target=\"_blank\" rel=\"noopener\" {}>{}</a></div>"
+    no_location_entry = "<div {}>\n<h3 style=\"color:#3c663b;text-align:left;margin-bottom:40%;\">{}</h3>\n<a href=\"http://{}\" target=\"_blank\" {}>{}</a></div>\n\n"
+
+
 
     #Skip entries with no company name or website
     if len( row[0] ) == 0 or len( row[4] ) == 0:
@@ -62,7 +75,7 @@ def row_to_div(row,fp):
         return entry.format(entry_style,row[4],row[3],row[2],row[1],row[0],link_style,"Visit")
     #No location data provided
     elif len(row[1]) + len(row[2]) + len(row[3]) == 0:
-        return no_location_entry.format(entry_style,row[0],row[4],link_style,"Visit")
+        return no_location_entry.format(entry_style,row[4],row[0],link_style,"Visit")
     #Partial location data - ZIP code provided
     elif (len(row[2]) == 0 or len(row[3]) == 0) and len(row[1]) > 0:
         #Grabs the city and state from Google's Geocoding api using the ZIP code
@@ -87,9 +100,13 @@ def row_to_div(row,fp):
         return False
 
 def export( fp ):
+    if not os.path.exists((target_dir+"/out")):
+        os.makedirs((target_dir+"/out"))
+    if not os.path.exists((target_dir+"/err")):
+        os.makedirs((target_dir+"/err"))
     #Create our output and error dump
-    exp = open((fp[:-4] + "_out.txt"),"w+")
-    err = open((fp[:-4] + "_err.csv"),"w+")
+    exp = open((target_dir+"/out/"+fp[:-4] + "_out.txt"),"w+")
+    err = open((target_dir+"/err/"+fp[:-4] + "_err.csv"),"w+")
     #Write parse results
     for elem in elements[fp]:
         exp.write( elem + "\n" )
@@ -112,20 +129,22 @@ def process(fp=False):
     export(fp)
 
 def process_all():
+    files = os.listdir(target_dir)
     #Process all .csv files in target directory(default program root)
-    for f in os.listdir(target_dir):
+    for f in files:
         if f[-4:] == ".csv":
+            print("Processing {}".format(f))
             process(f)
 
 print("---------------------------------------------------------------------\n-ITRM csv-html converter :: 2018 :: Jarek Troyer :: IT Risk Managers-\n---------------------------------------------------------------------")
-user_input = True
-while user_input != False:
-    user_input = input("\nEnter a command number:\n1) Convert all .csv in {}...\n2) Enter files to be converted...\n3) Change target_dir...\n4) Exit\n\n>".format(("this directory" if target_dir == "." else target_dir)))
+user_input = "z"
+while user_input != "4":
+    user_input = str(input("\nEnter a command number:\n1) Convert all .csv in {}...\n2) Enter files to be converted...\n3) Change target_dir...\n4) Exit\n\n>".format(("this directory" if target_dir == "." else target_dir))))
     #Process all files
-    if user_input == 1:
+    if user_input == "1":
         process_all()
     #Process specified files
-    elif user_input == 2:
+    elif user_input == "2":
         #Storage for our user-generated file list
         to_be_processed = []
         #Get target files
@@ -137,7 +156,5 @@ while user_input != False:
         for fp in to_be_processed:
             process(fp)
     #Change target directory
-    elif user_input == 3:
+    elif user_input == "3":
         target_dir = input("\nSet target directory:\n>")
-    elif user_input == 4:
-        user_input = False
